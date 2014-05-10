0b72c96f57ea1f9272b99802a98187e06c9a8ced
Valery Tolstov <testautomail@mail.ru>
2014-05-10 13:54:36 +0400

    Update run.h

--- include/run.h
+++ include/run.h
@@ -4,7 +4,8 @@
 #include <hashtable.h>
 
 void run_interactive_loop(const char *prompt, env_hashtable *env);
-void run_file(FILE *f, env_hashtable *env);
+void run_file(char *path, env_hashtable *env);
+void run_opened_file(FILE *f, env_hashtable *env);
 void run_expression(const char *expr, env_hashtable *env);
 
 #endif /* RUN_H */

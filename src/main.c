6b2cb30025e40b288e26223ac2affe0c55eed6b0
Valery Tolstov <testautomail@mail.ru>
2014-05-11 21:02:16 +0400

    Add possibility to parse options without getopt

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #include <run.h>
 #include <init.h>
@@ -9,41 +10,25 @@
 # include <getopt.h>
 #endif
 
+char *expr = NULL,
+     *prompt = NULL,
+     *in_file = NULL;
+
 void print_usage();
+void parse_options(int argc, char *argv[]);
 
 int main(int argc, char *argv[])
 {
     env_hashtable *global_env;
-    char *expr = NULL,
-         *prompt = NULL;
-    int opt, ind = 1;
     FILE *f;
     
-#ifdef HAVE_GETOPT
-    while ((opt = getopt(argc, argv, "e:hp:")) != -1){
-        switch (opt) {
-            case 'e':
-                expr = optarg;
-                break;
-            case 'p':
-                prompt = optarg;
-                break;
-            case 'h':
-            case '?':
-                print_usage();
-                exit(EXIT_FAILURE);
-            default:
-                break;
-        };
-    };
-    ind = optind;
-#endif
+    parse_options(argc, argv);
     
     global_env = env_hashtable_new();
     init_global_environment(global_env);
     
-    if (ind < argc) {
-        f = fopen(argv[ind], "r");
+    if (in_file) {
+        f = fopen(in_file, "r");
         if (!f) {
             perror("fopen");
             exit(EXIT_FAILURE);
@@ -62,4 +47,56 @@ void print_usage()
     printf("%s\n",
         "Usage: "
         PROGRAM_NAME " [-e <expression>]");
+}
+
+void parse_options(int argc, char *argv[])
+{
+#ifdef HAVE_GETOPT
+    int opt;
+    while ((opt = getopt(argc, argv, "e:hp:")) != -1){
+        switch (opt) {
+        case 'e':
+            expr = optarg;
+            break;
+        case 'p':
+            prompt = optarg;
+            break;
+        case 'h':
+        case '?':
+            print_usage();
+            exit(EXIT_FAILURE);
+        default:
+            break;
+        };
+    };
+    in_file = argv[optind];
+#else
+    int optind = 0, subind = 0;
+    size_t s_len;
+
+#define ARG_PARAMETER (subind == s_len - 1 ? argv[++optind] : argv[optind] + subind + 1)
+
+    optind++;
+    while (optind < argc) {
+        if (subind == 0) {
+            s_len = strlen(argv[optind]);
+            if (!s_len)
+                continue;
+            if (argv[optind][0] == '-')
+                subind++;
+            else
+                in_file = argv[optind];
+        } else if (argv[optind][subind] == 'e') {
+            expr = ARG_PARAMETER;
+            optind++;
+            subind = 0;
+        } else if (argv[optind][subind] == 'p') {
+            prompt = ARG_PARAMETER;
+            optind++;
+            subind = 0;
+        }
+    }
+
+#undef ARG_PARAMETER
+#endif
 }

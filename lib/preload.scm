345832f649f0ddde22b222a4324968e1cf596ebc
Valery Tolstov <testautomail@mail.ru>
2014-05-11 14:26:25 +0400

    Add some logical procedures

--- lib/preload.scm
+++ lib/preload.scm
@@ -1,1 +1,18 @@
 (define (null? x) (eqv? x '()))
+
+(define (not arg)
+    (if arg #f #t))
+
+(define (or . args)
+    (if (null? args)
+        #f
+        (if (car args)
+            (car args)
+            (apply or (cdr args)))))
+
+(define (and . args)
+    (if (null? args)
+        #t
+        (if (car args)
+            (apply and (cdr args))
+            #f)))
